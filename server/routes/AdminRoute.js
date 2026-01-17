b674b56f62704081064ad95e0fd1bd45081101ec
Santhosh-R1 <santhoshrajan817@gmail.com>
2026-01-17 14:25:39 +0530

    new one

--- server/routes/AdminRoute.js
+++ server/routes/AdminRoute.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { addEmployee, updateEmployeeStatus } = require("../controllers/Admin");
 router.post("/employee", addEmployee);
 router.put("/employee/:id/status", updateEmployeeStatus);
 
